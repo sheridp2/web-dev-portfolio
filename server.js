4348c7fd0041c71386a2ab587e9318391ae58d8e
Patrick Sheridan <sheridp2@gmail.com>
2017-07-31 18:14:48 -0700

    server.js added

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+const router = express.Router();
+
+app.use(express.static('./public'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
